cebf644914846fc2f783b608fbf66b3f9ee0e97d
ldscholar <ldscholar@.github.com>
2018-01-19 11:27:50 +0800

    实现简单功能的管道

---
+++ src/SimplePipeline.groovy
@@ -0,0 +1,45 @@
+//src/SimplePipeline.groovy
+def checkout(credentialsId, remoteUrl) {
+    def scm = [$class              : 'SubversionSCM',
+               filterChangelog     : false,
+               ignoreDirPropChanges: false,
+               includedRegions     : '',
+               locations           : [[credentialsId: "$credentialsId", depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "$remoteUrl"]],
+               quietOperation      : true,
+               workspaceUpdater    : [$class: 'UpdateUpdater']]
+    checkout(scm)
+
+    def changeLogSets = currentBuild.changeSets
+    if (null == changeLogSets || changLogSet.isEmpty()) {
+        env.isChanged = 'false'
+    } else {
+        env.isChanged = 'true'
+    }
+}
+
+def build() {
+    if (env.isChanged.equals('true')) {
+        sh 'mvn clean deploy'
+    }
+    def pom = readMavenPom file: 'pom.xml'
+    env.jar = pom.artifactId + '-' + pom.version + '.jar'
+    env.artifact = pom.parent.groupId + ':' + pom.artifactId + ':' + pom.version
+}
+
+def deploy(profile) {
+    sh "mvn dependency:get -DremoteRepositories=$NEXUS -Dartifact=$artifact -Ddest=$WORKSPACE"
+    try {
+        sh "ps -ef | grep $jar | grep -v grep | awk '{print \$2}' | xargs kill -9"
+    } catch (err) {
+        echo "WARNING: 旧服务关闭失败,可能是旧服务未启动或已关闭"
+    }
+
+    dir("$JAR_RUNNING_PATH") {
+        if (fileExists("$jar")) {
+            sh "mv -b ./$jar ./backup/$jar"
+        }
+        sh "mv -f $WORKSPACE/$jar ./$jar"
+        sh """JENKINS_NODE_COOKIE=dontKillMe
+                    setsid java -jar $jar --spring.profiles.active=$profile &"""
+    }
+}
