d63479a0a94943ad21eccf7d1c72a203ebc3f48e
ldscholar <ldscholar@.github.com>
2018-01-22 15:07:18 +0800

    test

--- vars/simplePipeline.groovy
+++ vars/simplePipeline.groovy
@@ -1,14 +1,16 @@
 //var/simplePipeline.groovy
 //import static utils.PipelineStage.*
 def checkout(String remoteUrl, String credentialsId) {
-    def scm = [$class              : 'SubversionSCM',
-               filterChangelog     : false,
-               ignoreDirPropChanges: false,
-               includedRegions     : '',
-               locations           : [[credentialsId: "$credentialsId", depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "$remoteUrl"]],
-               quietOperation      : true,
-               workspaceUpdater    : [$class: 'UpdateUpdater']]
-    checkout(scm)
+    node("dss-12"){
+        def scm = [$class              : 'SubversionSCM',
+                   filterChangelog     : false,
+                   ignoreDirPropChanges: false,
+                   includedRegions     : '',
+                   locations           : [[credentialsId: "$credentialsId", depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "$remoteUrl"]],
+                   quietOperation      : true,
+                   workspaceUpdater    : [$class: 'UpdateUpdater']]
+        checkout(scm)
+    }
 }
 
 def call(String buildServer, List<String> deployServers, String remoteUrl, String credentialsId, Map<String, String> profile) {
