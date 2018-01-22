448edffd90e9d5cc90b5734a590b8a4922453052
ldscholar <ldscholar@.github.com>
2018-01-22 14:01:26 +0800

    test

---
+++ vars/pipelineStage.groovy
@@ -0,0 +1,47 @@
+//src/PipelineStage.groovy
+class PipelineStage{
+    static def checkout(String remoteUrl, String credentialsId) {
+        def scm = [$class              : 'SubversionSCM',
+                   filterChangelog     : false,
+                   ignoreDirPropChanges: false,
+                   includedRegions     : '',
+                   locations           : [[credentialsId: "$credentialsId", depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "$remoteUrl"]],
+                   quietOperation      : true,
+                   workspaceUpdater    : [$class: 'UpdateUpdater']]
+        checkout(scm)
+
+        def changeLogSets = currentBuild.changeSets
+        if (null == changeLogSets || changeLogSets.isEmpty()) {
+            env.isChanged = 'false'
+        } else {
+            env.isChanged = 'true'
+        }
+    }
+
+    static def build() {
+        if (env.isChanged.equals('true')) {
+            sh 'mvn clean deploy'
+        }
+        def pom = readMavenPom file: 'pom.xml'
+        env.jar = pom.artifactId + '-' + pom.version + '.jar'
+        env.artifact = pom.parent.groupId + ':' + pom.artifactId + ':' + pom.version
+    }
+
+    static def deploy(Map profile) {
+        sh "mvn dependency:get -DremoteRepositories=$NEXUS -Dartifact=$artifact -Ddest=$WORKSPACE"
+        try {
+            sh "ps -ef | grep $jar | grep -v grep | awk '{print \$2}' | xargs kill -9"
+        } catch (err) {
+            echo "WARNING: 旧服务关闭失败,可能是旧服务未启动或已关闭"
+        }
+
+        dir("$JAR_RUNNING_PATH") {
+            if (fileExists("$jar")) {
+                sh "mv -b ./$jar ./backup/$jar"
+            }
+            sh "mv -f $WORKSPACE/$jar ./$jar"
+            sh """JENKINS_NODE_COOKIE=dontKillMe
+                        setsid java -jar $jar --spring.profiles.active=$profile &"""
+        }
+    }
+}
--- vars/simplePipeline.groovy
+++ vars/simplePipeline.groovy
@@ -17,58 +17,29 @@ def checkout(String remoteUrl, String credentialsId) {
     }
 }
 
-def build() {
-    if (env.isChanged.equals('true')) {
-        sh 'mvn clean deploy'
-    }
-    def pom = readMavenPom file: 'pom.xml'
-    env.jar = pom.artifactId + '-' + pom.version + '.jar'
-    env.artifact = pom.parent.groupId + ':' + pom.artifactId + ':' + pom.version
-}
-
-def deploy(Map profile) {
-    sh "mvn dependency:get -DremoteRepositories=$NEXUS -Dartifact=$artifact -Ddest=$WORKSPACE"
-    try {
-        sh "ps -ef | grep $jar | grep -v grep | awk '{print \$2}' | xargs kill -9"
-    } catch (err) {
-        echo "WARNING: 旧服务关闭失败,可能是旧服务未启动或已关闭"
-    }
-
-    dir("$JAR_RUNNING_PATH") {
-        if (fileExists("$jar")) {
-            sh "mv -b ./$jar ./backup/$jar"
-        }
-        sh "mv -f $WORKSPACE/$jar ./$jar"
-        sh """JENKINS_NODE_COOKIE=dontKillMe
-                    setsid java -jar $jar --spring.profiles.active=$profile &"""
-    }
-}
-
 def call(String buildServer, String remoteUrl, String credentialsId){
-    pipeline{
-        node(buildServer) {
-            stage('Checkout') {
-                //checkout(credentialsId, remoteUrl)
-                def scm = [$class              : 'SubversionSCM',
-                           filterChangelog     : false,
-                           ignoreDirPropChanges: false,
-                           includedRegions     : '',
-                           locations           : [[credentialsId: "$credentialsId", depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "$remoteUrl"]],
-                           quietOperation      : true,
-                           workspaceUpdater    : [$class: 'UpdateUpdater']]
-                checkout(scm)
+    node(buildServer) {
+        stage('Checkout') {
+            //checkout(credentialsId, remoteUrl)
+            def scm = [$class              : 'SubversionSCM',
+                       filterChangelog     : false,
+                       ignoreDirPropChanges: false,
+                       includedRegions     : '',
+                       locations           : [[credentialsId: "$credentialsId", depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "$remoteUrl"]],
+                       quietOperation      : true,
+                       workspaceUpdater    : [$class: 'UpdateUpdater']]
+            checkout(scm)
 
-                def changeLogSets = currentBuild.changeSets
-                if (null == changeLogSets || changeLogSets.isEmpty()) {
-                    env.isChanged = 'false'
-                } else {
-                    env.isChanged = 'true'
-                }
+            def changeLogSets = currentBuild.changeSets
+            if (null == changeLogSets || changeLogSets.isEmpty()) {
+                env.isChanged = 'false'
+            } else {
+                env.isChanged = 'true'
             }
+        }
 
-            stage('Build') {
-                //build()
-            }
+        stage('Build') {
+            //build()
         }
     }
 }
