af1ea760a3196195a648aa318c6bbbacba5b87e2
ldscholar <ldscholar@.github.com>
2018-01-22 14:03:05 +0800

    test

--- vars/simplePipeline.groovy
+++ vars/simplePipeline.groovy
@@ -20,22 +20,7 @@ def checkout(String remoteUrl, String credentialsId) {
 def call(String buildServer, String remoteUrl, String credentialsId){
     node(buildServer) {
         stage('Checkout') {
-            //checkout(credentialsId, remoteUrl)
-            def scm = [$class              : 'SubversionSCM',
-                       filterChangelog     : false,
-                       ignoreDirPropChanges: false,
-                       includedRegions     : '',
-                       locations           : [[credentialsId: "$credentialsId", depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "$remoteUrl"]],
-                       quietOperation      : true,
-                       workspaceUpdater    : [$class: 'UpdateUpdater']]
-            checkout(scm)
-
-            def changeLogSets = currentBuild.changeSets
-            if (null == changeLogSets || changeLogSets.isEmpty()) {
-                env.isChanged = 'false'
-            } else {
-                env.isChanged = 'true'
-            }
+            checkout(credentialsId, remoteUrl)
         }
 
         stage('Build') {
